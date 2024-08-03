63de0c7d706b3519779551da80d9c94077d8aa30
melange <34423707+melange@users.noreply.github.com>
2024-08-04 03:04:52 +0400

    135 candy

--- 135_candy.py
+++ 135_candy.py
@@ -1,9 +1,61 @@
 import unittest
 
-
 class Solution:
-    def candy(self, ratings: list[int]) -> int:
+    def candy(self, ratings) -> int:
+        candy = [1] * len(ratings)
+        for i in range(1, len(ratings)):
+            if ratings[i-1] < ratings[i]:
+                candy[i] = candy[i-1] + 1
         
+        for i in range(len(ratings)-2,-1,-1):
+            if ratings[i] > ratings[i+1]:
+                candy[i] = max(candy[i], candy[i+1] + 1)
+            
+        return sum(candy)
+
+
+class Solution2:
+    def candy(self, ratings) -> int:
+        graph = self.build_graph(ratings)
+        return self.calc_sum(graph, ratings)
+        
+    def build_graph(self, ratings):
+        graph = {
+            'root': []
+        }
+        for i, r in enumerate(ratings):
+            graph[i] = []
+            r = ratings[i]
+            if (i > 0) and (r > ratings[i-1]):
+                graph[i-1].append(i)
+            else:
+                if (i > 0) and (r < ratings[i-1]):
+                    graph['root'].append(i)
+                    graph[i].append(i-1)
+                    if (i-1) in graph['root']:
+                        graph['root'].remove(i-1)
+                else:
+                    graph['root'].append(i)
+        return graph
+    
+    def calc_sum(self, graph, ratings):
+        sum = 0
+        sums = {}
+        queue = [('root', 0)]
+        depth = 0
+        while queue:
+            node, depth = queue.pop(0)
+            if depth > 0:
+                if node not in sums:
+                    sums[node] = depth
+                    sum = sum + depth
+                else:
+                    sum = sum - sums[node] + depth
+                    sums[node] = depth
+            for neighbour in graph[node]:
+                queue.append((neighbour, depth+1))
+        return sum
+
 
 class TestSolution(unittest.TestCase):   
     def test_1(self):
@@ -42,3 +94,6 @@ class TestSolution(unittest.TestCase):
         correct_answer = 47
         solution = Solution()
         self.assertEqual(solution.candy(ratings), correct_answer)
+        
+        
+unittest.main()
